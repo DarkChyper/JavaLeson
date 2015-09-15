31eeceb67cc10060414c96a28e884c800f011b1a
DarkChyper <contact@simon-lhoir.fr>
2015-09-15 14:02:07 +0200

    P3C1 les fenêtres

---
+++ P3C1_FENETRE/src/premiere_fenetre/Fenetre.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package premiere_fenetre;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * @author dark
+ * 
+ */
+@SuppressWarnings("serial")
+public class Fenetre extends JFrame {
+	/**
+	 * Constructeur par défaut
+	 */
+	public Fenetre() {
+		this.setTitle("Ma première fenêtre Java");
+		this.setSize(400, 500);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setUndecorated(false);
+
+		final JPanel panel = new JPanel();
+		panel.setBackground(Color.ORANGE);
+
+		this.setContentPane(panel);
+
+		this.setVisible(true);
+	}
+
+	/**
+	 * Constructeur avec paramètres
+	 * 
+	 * @param titre
+	 * @param longueur
+	 * @param largeur
+	 */
+	public Fenetre(final String titre, final int longueur, final int largeur,
+			final boolean resize, final boolean decor) {
+		this.setTitle(titre);
+		this.setSize(longueur, largeur);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(resize);
+		this.setUndecorated(decor);
+		this.setVisible(true);
+	}
+}
---
+++ P3C1_FENETRE/src/premiere_fenetre/Test.java
@@ -0,0 +1,15 @@
+package premiere_fenetre;
+
+
+/**
+ * @author dark
+ * 
+ */
+public class Test {
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		final Fenetre fen = new Fenetre();
+	}
+}
